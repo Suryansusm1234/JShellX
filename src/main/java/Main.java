190c294fc112987497b66625d62b9e1c6c336093
Suryansusm1234 <ssanjeebanimohanty@gmail.com>
2026-07-31 22:02:22 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void Echofunction(String input){
+        String result =  input.replace("echo ", "");
+       System.out.println(result);
+       return;
+    }
+
     public static void main(String[] args) throws Exception {
         Scanner sc =  new Scanner(System.in);
         while(true){
             System.out.print("$ ");
-        String input =  sc.next();
-        if (!input.equals("exit")){
-           System.out.println(input + ": command not found" );
-       }else{
-        break;
+        String input =  sc.nextLine();
+        if (input.equals("exit")){
+           break;
+       }else{  
+        if (input.startsWith("echo")) {
+            Echofunction(input);
+        }
        }
         }
 
