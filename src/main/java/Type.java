1f09a5ea365244b6399b80e7fd1a82643a14abaa
Suryansusm1234 <ssanjeebanimohanty@gmail.com>
2026-08-01 22:52:43 +0530

    Refactor directory_Search method and streamline type method logic

--- src/main/java/Type.java
+++ src/main/java/Type.java
@@ -9,27 +9,29 @@ public class Type {
         buildin.add("exit");
     
     }
-
+    public static String directory_Search(String input){
+        String [] searchDIR  = System.getenv("PATH").split(":");
+         for(int i = 0; i < searchDIR.length ; i++){
+                String candidateString = searchDIR[i]+"/"+input;
+                File file = new File(candidateString);
+                
+                if (file.exists()&& file.isFile() && file.canExecute()) {
+                    return candidateString;
+                }
+                
+            }
+             return null;
+    }
     public static void type(String input) {
         if (buildin.contains(input)) {
             System.out.println(input + " is a shell builtin");
             return;
         } else {
-           String [] SearchDIR  = System.getenv("PATH").split(":");
-           boolean wasfound = false;
-            for(int i = 0; i < SearchDIR.length ; i++){
-                String candidateString = SearchDIR[i]+"/"+input;
-                File file = new File(candidateString);
-                
-                if (file.exists()&& file.isFile() && file.canExecute()) {
-                    System.out.println(input + " is " + candidateString);
-                    wasfound = true;
-                    break;
-                }
-                
-            }
-            if (!wasfound) {
-                System.out.println(input+": not found");
+            String candidateString = directory_Search(input);
+            if (candidateString != null) {
+                System.out.println(input+" is "+ candidateString);
+                }else{
+                    System.out.println(input+": is not found");
                 }
             return;
         }
